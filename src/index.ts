4ed98932373ee0e8d57b718a49317375d1f0f5d1
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-08-21 20:34:10 -0400

    Update the interface for `ServiceContainer`

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,39 @@
 import { ClassDecContext, DecoratedClass } from './models/decorators.js';
 import CtorParser from './services/CtorParser.js';
 
-export class ServiceContainer {
+export interface IServiceContainer {
+	/** Get the instance of a service by the service name */
+	get<T>(name: string): T;
+	/** Get the instance of a service by the constructor */
+	get<T>(ctor: new (...args: any[]) => T): T;
+	/** Set the instance for a service identified by a name */
+	set<T>(name: string, service: T): void;
+	/** Set the instance for a service identified by the constructor */
+	set<T>(ctor: new (...args: any[]) => T, service: T): void;
+	/** Define a class which */
+	defineService<T>(ctor: new (...args: any[]) => T, depedencies: string[]): void;
+	/** Define a function which produces an instance of a service */
+	defineFactory<T>(factory: () => T, name?: string): void;
+	/** Define a way of creating a new instance of a service from an existing instance in a parent container */
+	refineFactory<T>(ctor: new (...args: any[]) => T, refiner: (input: T) => T): void;
+}
+
+export class ServiceContainer implements IServiceContainer {
 	/** A mapping of a service name to its factory */
-	private readonly nameToFactory: Map<string, () => any> = new Map();
+	private readonly factoryLookup: Map<string, () => any> = new Map();
 
 	/** A mapping of a service name to the current instance */
-	private readonly nameToInstance: Map<string, any> = new Map();
-
-	/** A mapping of a service constructor to the service name */
-	private readonly ctorToName: Map<new (...args: any[]) => any, string> = new Map();
+	private readonly instanceLookup: Map<string | (new (...args: any[]) => any), any> = new Map();
 
 	/** The object used to parse information from a constructor definition */
 	private ctorParser = new CtorParser();
 
 	constructor(private parent?: ServiceContainer) { }
 
-	/** Get a service by its name */
-	get<T>(name: string): T;
-
-	/** Geta  service by its constructor */
-	get<T>(ctor: new (...args: any[]) => T): T;
-
 	get<T>(nameOrCtor: string | (new (...args: any) => T)): T {
 		// Resolve the name if ctor was provided
 		if (typeof nameOrCtor !== 'string') {
-			const name = this.ctorToName.get(nameOrCtor);
+			const name = this.makeServiceId(nameOrCtor.name);
 			if (name === undefined) {
 				throw new Error(`The service '${nameOrCtor.name}' has not been defined`);
 			}
@@ -35,41 +43,61 @@ export class ServiceContainer {
 
 	}
 
-	/** Provide an instantiated instance of a service */
-	set<T>(name: string, service: T): void {
-		this.nameToInstance.set(name, service);
+	set<T>(name: string | (new (...args: any[]) => T), service: T): void {
+		this.instanceLookup.set(name, service);
 	}
 
-	defineService<T>(name: string, ctor: new (...args: any[]) => T, dependencies: string[]): void {
-		this.ctorToName.set(ctor, name);
-		this.nameToFactory.set(name, (): T => {
+	defineService<T>(ctor: new (...args: any[]) => T, dependencies: string[]): void {
+		const name = this.makeServiceId(ctor.name);
+		this.defineFactory((): T => {
 			const args: any[] = [];
 			for (const dep of dependencies) {
 				args.push(this.getService(dep));
 			}
 
 			return new ctor(...args);
-		});
+		}, name);
 	}
 
-	readonly register = <T>(value: DecoratedClass<T>, context: ClassDecContext) => {
-		const serviceId = this.makeServiceId(context.name);
+	defineFactory<T>(factory: () => T, name?: string) {
+		if (name === undefined) {
+			name = this.makeServiceId(factory.name);
+			if (name === '') {
+				throw new Error('Unable to determine the name of the factory: ' + factory.toString());
+			}
+		}
+
+		this.factoryLookup.set(name, factory);
+	}
+
+	refineFactory<T>(ctor: new (...args: any[]) => T, refiner: (input: T) => T) {
+		const name = this.makeServiceId(ctor.name);
+		this.defineFactory(() => {
+			if (this.parent === undefined) {
+				throw new Error('refineFactory can only be invoked on a ServiceContainer which has a parent');
+			}
+
+			const existing = this.parent.get(ctor);
+			const refined = refiner(existing);
+			return refined;
+		}, name)
+	}
+
+	readonly register = <T>(value: DecoratedClass<T>) => {
 		const dependencies = this.ctorParser.parseCtor(value);
-
-		this.defineService(serviceId, value as unknown as (new (...args: any[]) => any), dependencies);
+		this.defineService(value as unknown as (new (...args: any[]) => any), dependencies);
 	}
-
 
 	private makeServiceId(name: string) {
 		return name.charAt(0).toLowerCase() + name.slice(1);
 	}
 
 	private getService<T>(name: string): T {
-		let instance = this.nameToInstance.get(name);
+		let instance = this.instanceLookup.get(name);
 
 		// Create the instance
 		if (instance === undefined) {
-			const ctor = this.nameToFactory.get(name);
+			const ctor = this.factoryLookup.get(name);
 			if (ctor === undefined) {
 				if (this.parent !== undefined) {
 					return this.parent.getService<T>(name);
@@ -79,7 +107,7 @@ export class ServiceContainer {
 			}
 
 			instance = ctor();
-			this.nameToInstance.set(name, instance);
+			this.instanceLookup.set(name, instance);
 		}
 
 		return instance;
