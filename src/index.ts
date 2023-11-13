53ebe46c4fa55aa18ea646d9d86fea1109eb4bba
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-11-13 09:20:13 -0500

    Allows for asynchronous initialization of services
    Updates the `ServiceContainer` implementation to allow for asynchronous
    factories. This allows for asynchronous initialization in cases such as
    connecting to a DB as part of initialization.

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,45 @@
-import { ClassDecContext, DecoratedClass } from './models/decorators.js';
 import { parseConstructorArgs } from './utils/constructor-parsing.js';
 import { parseFunctionArgs } from './utils/function-parsing.js';
 
 export interface IServiceContainer {
 	/** Get the instance of a service by the service name */
-	get<T>(name: string): T;
+	get<T>(name: string): Promise<T>;
 	/** Get the instance of a service by the constructor */
-	get<T>(ctor: new (...args: any[]) => T): T;
+	get<T>(ctor: Service<T>): Promise<T>;
 	/** Set the instance for a service identified by a name */
-	set<T>(name: string, service: T): void;
+	set<T>(name: string, service: T): IServiceContainer;
 	/** Set the instance for a service identified by the constructor */
-	set<T>(ctor: new (...args: any[]) => T, service: T): void;
+	set<T>(ctor: Service<T>, service: T): IServiceContainer;
 	/** Define a class which */
-	defineService<T>(ctor: new (...args: any[]) => T, depedencies: string[]): void;
+	defineService<T>(ctor: Service<T>): IServiceContainer;
 	/** Define a function which produces an instance of a service */
-	defineFactory<T>(factory: (...args: any[]) => T, name?: string): void;
+	defineFactory<T>(name: string, factory: Factory<T>): IServiceContainer;
+	/** Define a named function which produces an instance of a service */
+	defineFactory<T>(factory: Factory<T>): IServiceContainer;
 	/** Define a way of creating a new instance of a service from an existing instance in a parent container */
-	refineFactory<T>(ctor: new (...args: any[]) => T, refiner: (input: T) => T): void;
+	refineService<T>(ctor: Service<T>, refiner: Refiner<T>): IServiceContainer;
 }
 
+type Factory<T> = (...args: any[]) => T | Promise<T>;
+
+type Refiner<T> = (input: T) => T | Promise<T>;
+
+type Service<T> = new (...args: any[]) => T;
+
 export class ServiceContainer implements IServiceContainer {
-	/** A mapping of a service name to its factory */
-	private readonly factoryLookup: Map<string, () => any> = new Map();
 
 	/** A mapping of a service name to the current instance */
-	private readonly instanceLookup: Map<string, any> = new Map();
+	private readonly instanceLookup: Map<string, unknown> = new Map();
+
+	/** A mapping of service name to pending initializations */
+	private readonly promiseLookup: Map<string, Promise<unknown>> = new Map();
+
+	/** A mapping of a service name to its factory */
+	private readonly factoryLookup: Map<string, () => (unknown | Promise<unknown>)> = new Map();
 
 	constructor(private parent?: ServiceContainer) { }
 
-	get<T>(nameOrCtor: string | (new (...args: any) => T)): T {
+	get<T>(nameOrCtor: string | Service<T>): Promise<T> {
 		// Resolve the name if ctor was provided
 		if (typeof nameOrCtor !== 'string') {
 			const name = this.makeServiceId(nameOrCtor.name);
@@ -42,28 +53,41 @@ export class ServiceContainer implements IServiceContainer {
 		return this.getService(nameOrCtor);
 	}
 
-	set<T>(name: string | (new (...args: any[]) => T), service: T): void {
+	set<T>(name: string | Service<T>, service: T): this {
 		if (typeof name !== 'string') {
 			name = this.makeServiceId(name.name);
 		}
 
 		this.instanceLookup.set(name, service);
+		return this;
 	}
 
-	defineService<T>(ctor: new (...args: any[]) => T): void {
+	defineService<T>(ctor: Service<T>): this {
 		const name = this.makeServiceId(ctor.name);
 		const dependencies = parseConstructorArgs(ctor);
-		this.defineFactory((): T => {
+		return this.defineFactory<T>(name, (): T => {
 			const args: any[] = [];
 			for (const dep of dependencies) {
 				args.push(this.getService(dep));
 			}
 
 			return new ctor(...args);
-		}, name);
+		});
 	}
 
-	defineFactory<T>(factory: (...args: any[]) => T, name?: string) {
+	defineFactory<T>(name: string, factory: Factory<T>): this;
+	defineFactory<T>(factory: Factory<T>): this;
+	defineFactory<T>(nameOrFactory: Factory<T> | string, optionalFactory?: Factory<T>): this {
+		let name: string;
+		let factory: Factory<T>;
+		if (typeof nameOrFactory === 'function') {
+			name = nameOrFactory.name;
+			factory = optionalFactory!;
+		} else {
+			name = nameOrFactory;
+			factory = optionalFactory!;
+		}
+
 		if (name === undefined) {
 			name = this.makeServiceId(factory.name);
 			if (name === '') {
@@ -73,56 +97,100 @@ export class ServiceContainer implements IServiceContainer {
 
 		const dependencies = parseFunctionArgs(factory);
 
-		this.factoryLookup.set(name, (): T => {
+		this.factoryLookup.set(name, async (): Promise<T> => {
+			// Determine the dependencies of the 
 			const args: any[] = [];
 			for (const dep of dependencies) {
-				args.push(this.getService(dep));
+				args.push(await this.getService(dep));
 			}
 
-			return factory(...args);
+			const instance = factory(...args);
+			if (instance instanceof Promise) {
+				return instance;
+			} else {
+				return Promise.resolve(instance);
+			}
+		});
+
+		return this;
+	}
+
+	refineService<T>(ctor: Service<T>, refiner: Refiner<T>): this {
+		const name = this.makeServiceId(ctor.name);
+		return this.defineFactory<T>(name, async (): Promise<T> => {
+			if (this.parent === undefined) {
+				throw new Error('The refineService method can only be invoked on a ServiceContainer which has a parent');
+			}
+
+			const existing = await this.parent.get(ctor);
+			const refined = refiner(existing);
+			if (refined instanceof Promise) {
+				return refined;
+			} else {
+				return Promise.resolve(refined);
+			}
 		});
 	}
 
-	refineFactory<T>(ctor: new (...args: any[]) => T, refiner: (input: T) => T) {
-		const name = this.makeServiceId(ctor.name);
-		this.defineFactory(() => {
+	refineFactory<T>(name: string, refiner: Refiner<T>): this;
+	refineFactory<T>(factory: Factory<T>, refiner: Refiner<T>): this;
+	refineFactory<T>(nameOrFactory: string | Factory<T>, refiner: Refiner<T>): this {
+		const name = typeof nameOrFactory === 'string' ? nameOrFactory : nameOrFactory.name;
+		return this.defineFactory<T>(name, async (): Promise<T> => {
 			if (this.parent === undefined) {
-				throw new Error('refineFactory can only be invoked on a ServiceContainer which has a parent');
+				throw new Error('The refineService method can only be invoked on a ServiceContainer which has a parent');
 			}
 
-			const existing = this.parent.get(ctor);
+			const existing = await this.parent.get<T>(name);
 			const refined = refiner(existing);
-			return refined;
-		}, name)
-	}
-
-	readonly register = <T>(value: DecoratedClass<T>) => {
-		this.defineService(value as unknown as (new (...args: any[]) => any));
+			if (refined instanceof Promise) {
+				return refined;
+			} else {
+				return Promise.resolve(refined);
+			}
+		});
 	}
 
 	private makeServiceId(name: string) {
 		return name.charAt(0).toLowerCase() + name.slice(1);
 	}
 
-	private getService<T>(name: string): T {
-		let instance = this.instanceLookup.get(name);
+	private async getService<T>(name: string): Promise<T> {
 
-		// Create the instance
-		if (instance === undefined) {
-			const ctor = this.factoryLookup.get(name);
-			if (ctor === undefined) {
-				if (this.parent !== undefined) {
-					return this.parent.getService<T>(name);
-				} else {
-					throw new Error(`The service with id '${name}' has not been defined`);
-				}
-			}
-
-			instance = ctor();
-			this.instanceLookup.set(name, instance);
+		// Check for a cached instance first
+		const existingInstance = this.instanceLookup.get(name);
+		if (existingInstance !== undefined) {
+			return Promise.resolve(existingInstance as T);
 		}
 
-		return instance;
+		// Check for a pending initialization
+		let promise = this.promiseLookup.get(name);
+		if (promise !== undefined) {
+			return promise as Promise<T>;
+		}
+
+		// Fetch the factory
+		const factory = this.factoryLookup.get(name);
+		if (factory === undefined) {
+			if (this.parent !== undefined) {
+				return this.parent.getService<T>(name);
+			} else {
+				throw new Error(`The service with id '${name}' has not been defined`);
+			}
+		}
+
+		// Create the instance
+		const factoryResult = factory();
+		if (factoryResult instanceof Promise) {
+			this.promiseLookup.set(name, factoryResult);
+			const builtInstance = await factoryResult;
+			this.promiseLookup.delete(name);
+			this.instanceLookup.set(name, builtInstance);
+			return builtInstance;
+		} else {
+			this.instanceLookup.set(name, factoryResult);
+			return Promise.resolve(factoryResult as T);
+		}
 	}
 }
 
