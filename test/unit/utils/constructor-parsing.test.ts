9e2bef1e3627920bebfd62e455cb33f7027f60a0
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-06-25 08:56:09 -0400

    Reorganizes existing tests

--- test/unit/utils/constructor-parsing.test.ts
+++ test/unit/utils/constructor-parsing.test.ts
@@ -1,59 +1,62 @@
 import assert from 'node:assert';
-import { it } from 'node:test';
+import test from 'node:test';
 import { parseConstructorArgs } from '../../../src/utils/constructor-parsing.js';
 
-it('parses no constructor', () => {
-	class Foo { }
+await test.describe('Constructor Parsing', async () => {
+	await test('no constructor', () => {
+		class Foo { }
 
-	const deps = parseConstructorArgs(Foo);
+		const deps = parseConstructorArgs(Foo);
 
-	assert.deepStrictEqual(deps, []);
-});
+		assert.deepStrictEqual(deps, []);
+	});
 
-it('parses empty constructor', () => {
-	class Foo {
-		constructor() { }
-	}
-
-	const deps = parseConstructorArgs(Foo);
-
-	assert.deepStrictEqual(deps, []);
-});
-
-it('parses single param', () => {
-	class Foo {
-		constructor(public foo: number) {
-
+	await test('empty constructor', () => {
+		class Foo {
+			constructor() { }
 		}
-	}
 
-	const deps = parseConstructorArgs(Foo);
+		const deps = parseConstructorArgs(Foo);
 
-	assert.deepStrictEqual(deps, ['foo']);
+		assert.deepStrictEqual(deps, []);
+	});
+
+	await test('single param', () => {
+		class Foo {
+			constructor(public foo: number) {
+
+			}
+		}
+
+		const deps = parseConstructorArgs(Foo);
+
+		assert.deepStrictEqual(deps, ['foo']);
+	});
+
+	await test('two params', () => {
+
+		class Foo {
+			constructor(public foo: number, public bar: string) {
+
+			}
+		}
+
+		const deps = parseConstructorArgs(Foo);
+
+		assert.deepStrictEqual(deps, ['foo', 'bar']);
+	});
+
+	await test('three params', () => {
+
+		class Foo {
+			constructor(public foo: number, public bar: string, public baz: boolean) {
+
+			}
+		}
+
+		const deps = parseConstructorArgs(Foo);
+
+		assert.deepStrictEqual(deps, ['foo', 'bar', 'baz']);
+	})
 });
 
-it('parses two params', () => {
-
-	class Foo {
-		constructor(public foo: number, public bar: string) {
-
-		}
-	}
-
-	const deps = parseConstructorArgs(Foo);
-
-	assert.deepStrictEqual(deps, ['foo', 'bar']);
-});
-
-it('parses three params', () => {
-
-	class Foo {
-		constructor(public foo: number, public bar: string, public baz: boolean) {
-
-		}
-	}
-
-	const deps = parseConstructorArgs(Foo);
-
-	assert.deepStrictEqual(deps, ['foo', 'bar', 'baz']);
-})
--- test/unit/utils/function-parsing.test.ts
+++ test/unit/utils/function-parsing.test.ts
@@ -1,49 +1,52 @@
 import assert from 'assert';
-import { it } from 'node:test';
+import test from 'node:test';
 import { parseFunctionArgs } from '../../../src/utils/function-parsing.js';
 
-it('parses args of arrow function', () => {
-	const result = parseFunctionArgs((a, b, c) => {
-		console.log(a, b, c);
-	});
-	assert.deepStrictEqual(result, ['a', 'b', 'c']);
-});
-
-it('parses args of anonymous function', () => {
-	const result = parseFunctionArgs(function (a, b, c) {
-		console.log(a, b, c);
+await test.describe('Function Parsing', async () => {
+	test('arrow function', () => {
+		const result = parseFunctionArgs((a, b, c) => {
+			console.log(a, b, c);
+		});
+		assert.deepStrictEqual(result, ['a', 'b', 'c']);
 	});
 
-	assert.deepStrictEqual(result, ['a', 'b', 'c']);
-});
+	test('arrow function with parentheses', () => {
+		const result = parseFunctionArgs(x => ({ x }))
+		assert.deepStrictEqual(result, ['x']);
+	});
 
-it('parses args of named function', () => {
-	function foo(a: unknown, b: unknown, c: unknown) {
-		console.log(a, b, c);
-	}
+	test('anonymous function', () => {
+		const result = parseFunctionArgs(function (a, b, c) {
+			console.log(a, b, c);
+		});
 
-	const result = parseFunctionArgs(foo);
-	assert.deepStrictEqual(result, ['a', 'b', 'c']);
-});
+		assert.deepStrictEqual(result, ['a', 'b', 'c']);
+	});
 
-it('parses no args of arrow function', () => {
-	const result = parseFunctionArgs(() => {});
-	assert.deepStrictEqual(result, []);
-});
+	test('named function', () => {
+		function foo(a: unknown, b: unknown, c: unknown) {
+			console.log(a, b, c);
+		}
 
-it('parses no parentheses of arrow function', () => {
-	const result = parseFunctionArgs(x => ({ x }))
-	assert.deepStrictEqual(result, ['x']);
-});
+		const result = parseFunctionArgs(foo);
+		assert.deepStrictEqual(result, ['a', 'b', 'c']);
+	});
 
-it('parses no args of anonymous function', () => {
-	const result = parseFunctionArgs(function() {});
-	assert.deepStrictEqual(result, []);
-});
+	test('empty arrow function', () => {
+		const result = parseFunctionArgs(() => {});
+		assert.deepStrictEqual(result, []);
+	});
 
-it('parses no args of named function', () => {
-	function foo() {}
-	const result = parseFunctionArgs(foo);
-	assert.deepStrictEqual(result, []);
+	test('empty anonymous function', () => {
+		const result = parseFunctionArgs(function() {});
+		assert.deepStrictEqual(result, []);
+	});
+
+	test('empty named function', () => {
+		function foo() {}
+		const result = parseFunctionArgs(foo);
+		assert.deepStrictEqual(result, []);
+	});
+
 });
 
